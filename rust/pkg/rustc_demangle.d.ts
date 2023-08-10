84d09eac14c59d182d291fd01e9af15a83a409fd
Raiki Tamura <tamaron1203@gmail.com>
2023-08-11 01:28:30 +0900

    Add rust build output

---
+++ rust/pkg/rustc_demangle.d.ts
@@ -0,0 +1,11 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} name
+*/
+export function greet(name: string): void;
+/**
+* @param {string} s
+* @returns {string}
+*/
+export function rust_demangle(s: string): string;
---
+++ rust/pkg/rustc_online_demangler_lib.js
@@ -0,0 +1,4 @@
+import * as wasm from "./rustc_online_demangler_lib_bg.wasm";
+import { __wbg_set_wasm } from "./rustc_online_demangler_lib_bg.js";
+__wbg_set_wasm(wasm);
+export * from "./rustc_online_demangler_lib_bg.js";
