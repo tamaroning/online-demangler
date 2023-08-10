e9d0b7bdb85effc23702cb93c1d2c78d58f8dc88
Raiki Tamura <tamaron1203@gmail.com>
2023-08-11 00:13:58 +0900

    initial commit

---
+++ rust/src/lib.rs
@@ -0,0 +1,13 @@
+use wasm_bindgen::prelude::*;
+
+// TODO: Add demanler function
+
+#[wasm_bindgen]
+extern {
+    pub fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet(name: &str) {
+    alert(&format!("Hello, {}!", name));
+}
